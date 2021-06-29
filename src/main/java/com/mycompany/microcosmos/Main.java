fa4baeee011b410f5af021f10509da14c4f2eb2a
ArielTorres <arielt2013@hotmail.com>
2021-06-28 19:48:29 -0600

    Repositorio creado.

---
+++ src/main/java/com/mycompany/microcosmos/Main.java
@@ -0,0 +1,15 @@
+/*Software Creado por: Gustavo Elvir
+                       David Escobar
+                       Cristhian Castro
+*/
+package com.mycompany.microcosmos;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+
+    }
+}
